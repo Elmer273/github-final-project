e79ab9879bb8ab390112b1132aecf2d8151d5ed9
Elmer Martinez <123431886+Elmer273@users.noreply.github.com>
2023-10-23 14:57:13 -0700

    Add git username

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # Elmer273
    # Input:
    # p, principal amount
    # t, time period in years
